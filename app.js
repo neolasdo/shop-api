3d711c6c6f57b7598a8ed8b2635642d65b016efb
Neolasdo <kikikun210195@gmail.com>
2018-05-16 20:35:17 +0700

    change app config header

--- app.js
+++ app.js
@@ -16,12 +16,9 @@ router.use(bodyParser.json());
 // Include all routes here
 app.all('*', function(req, res, next) {
      var origin = req.get('origin'); 
-     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Origin", origin);
      res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-     res.header('Access-Control-Allow-Headers', 'Origin');
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     res.header('Access-Control-Allow-Headers', 'Authorization');
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
      next();
 });
 var apiRoute = require('./routes/api');

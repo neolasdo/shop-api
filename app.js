534c9772c0601fa1235eb3cc1891f6a1f3802715
Tuan <tuannd@gemvietnam.com>
2018-05-16 15:01:55 +0700

    add  access-control-allow

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ router.use(bodyParser.json());
 app.all('*', function(req, res, next) {
      var origin = req.get('origin'); 
      res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
      next();
 });
 var apiRoute = require('./routes/api');

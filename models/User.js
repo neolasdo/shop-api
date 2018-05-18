329472958e3ff1a0ff9018a005cfa25bf1f45bb4
Tuan <tuannd@gemvietnam.com>
2018-05-18 09:13:03 +0700

    User model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,114 @@
 var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+var crypto = require('crypto'),
+    Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-
+    firstName: {
+        type: String,
+        trim: true,
+        default: '',
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        default: '',
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        default: '',
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        default: ''
+    },
+    salt: {
+        type: String
+    },
+    profileImageURL: {
+        type: String,
+        default: 'public/img/profile/default'
+    },
+    updated: {
+        type: Date
+    },
+    provider: {
+        type: String,
+        default: 'local'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    /* For reset password */
+    resetPasswordToken: {
+        type: String
+    },
+    resetPasswordExpires: {
+        type: Date
+    }
 })
+/**
+ * Hook a pre save method to hash the password
+ */
+UserSchema.pre('save', function (next) {
+    if (this.password && this.isModified('password')) {
+        this.salt = crypto.randomBytes(16).toString('base64');
+        this.password = this.hashPassword(this.password);
+    }
+
+    next();
+});
+
+
+/**
+ * Create instance method for hashing a password
+ */
+UserSchema.methods.hashPassword = function (password) {
+    if (this.salt && password) {
+        return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'base64'), 10000, 64).toString('base64');
+    } else {
+        return password;
+    }
+};
+/**
+ * Create instance method for authenticating user
+ */
+UserSchema.methods.authenticate = function (password) {
+    return this.password === this.hashPassword(password);
+};
+
+/**
+ * Find possible not used username
+ */
+/*
+UserSchema.statics.findUniqueUsername = function (username, suffix, callback) {
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+
+    _this.findOne({
+        username: possibleUsername
+    }, function (err, user) {
+        if (!err) {
+            if (!user) {
+                callback(possibleUsername);
+            } else {
+                return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+            }
+        } else {
+            callback(null);
+        }
+    });
+};
+*/
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
